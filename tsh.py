608b08843d70f2eda34759973858b95cdc63558a
Andrew <patch010@gmail.com>
2014-07-05 15:30:24 +1000

    First attempt at probability method

--- tsh.py
+++ tsh.py
@@ -1,9 +1,11 @@
 import numpy as np
 import numpy.linalg as npla
 import numpy.random as npr
+from numpy.testing import assert_allclose
 import math
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+np.set_printoptions(precision=2)
 
 def num2str(n):
     assert 0 <= n <= 99
@@ -81,16 +83,66 @@ def minmax(iterations):
        reduce the domain of each letter using the domain of all other letters. Repeat for the given number of iterations.
        Results in reduction of problem size from 2^173 to 2^73, converges after about 4 iterations."""
     domain = [slice(0, 100) for letter in alphabet]
-    print("Start: problem size 2^%s" % math.ceil(math.log(domain_size(domain), 2)))
+#     print("Start: problem size 2^%s" % math.ceil(math.log(domain_size(domain), 2)))
     for iteration in range(iterations):
         for i in range(len(alphabet)):
             i_counts = [letter_counts[domain[j], i] for j in range(len(alphabet))]
             domain[i] = slice(sum(np.amin(x) for x in i_counts) + b[i], sum(np.amax(x) for x in i_counts) + 1 + b[i])
-        print("Iteration %s: problem size 2^%s" % (iteration, math.ceil(math.log(domain_size(domain), 2))))
-        print(", ".join(
+#         print("Iteration %s: problem size 2^%s" % (iteration, math.ceil(math.log(domain_size(domain), 2))))
+    print(", ".join(
             "%s %s %s" % (alphabet[i], domain[i].start, domain[i].stop) if domain[i].stop != domain[i].start + 1
             else "%s=%s" % (alphabet[i], domain[i].start) for i in range(len(alphabet))))
     return domain
+
+def graph_p(p, letter_idx):
+    pass
+
+def letter_prob(p, letter):
+    """Input: probability of each value
+       Output: probability of each letter count"""
+    assert_allclose(np.sum(p), 1)
+    buckets = np.zeros(100)
+    for n in range(p.size):
+        buckets[letter_counts[n, letter]] += p[n]
+    assert_allclose(np.sum(buckets), 1)
+    return buckets
+
+def prob_sum(p1, p2):
+    """Probability distribution of sum of two random variables. See http://www.statlect.com/sumdst1.htm."""
+    # We assume sum will never be greater than 99 so p will always have enough elements - this is
+    # equivalent to assuming no values in solution are greater than 99
+    assert_allclose(np.sum(p1), 1)
+    assert_allclose(np.sum(p2), 1)
+    p = np.zeros_like(p1)
+    for n in range(p.size):
+        p[n] = sum(p1[x] * p2[n - x] for x in range(0, n + 1))
+    assert_allclose(np.sum(p), 1)
+    return p
+
+def prob(iterations):
+    domain = minmax(10)
+    # Initialise each letter to uniform distribution
+    p_mat = np.zeros_like(letter_counts, dtype='float')
+    for i in range(len(alphabet)):
+        p_mat[domain[i], i] = 1/(domain[i].stop - domain[i].start)
+
+    for iteration in range(iterations):
+        for i in range(len(alphabet)):
+            # Calculate new distribution for letter i
+            # Start with [1, 0, ..., 0] (length 100), then repeatedly update using rule for sum of random variables.
+            # Result is that each entry of p_total is the probability of letter i being that number based on current
+            # distribution stored in p
+            p_total = np.zeros(letter_counts.shape[0])
+            p_total[0] = 1 # [1, 0, 0, ..., 0] is the identity element for prob_sum
+            for j in range(len(alphabet)):
+                p_total = prob_sum(p_total, letter_prob(p_mat[:, j], i))
+
+            # np.roll shifts array elements right a certain number of places - this is necessary for the bias b
+            p_mat[:, i] = np.roll(p_total, b[i])
+    return p_mat[:, 4] # e
+
+
+
 
 def iter(v, iterations):
     """Repeat f(f(...f(v))) for a given number of iterations, return best v seen"""
